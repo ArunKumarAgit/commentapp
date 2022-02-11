6c4fd88932be2925d41f9a4901ed650a897a1d88
aarunkumar5044@gmail.com <aarunkumar5044@gmail.com>
2022-02-12 00:07:39 +0530

    comment

---
+++ index.js
@@ -0,0 +1,155 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('commentapp'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/mydb',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"));
+
+//get request
+app.get('/sign_up',async(req,res) => {
+    try{
+           const aliens = await Alien.find()
+           res.json(aliens)
+    }catch(err){
+        res.send('Error ' + err)
+    }
+})
+
+app.get("/reset",(req,res)=>{
+    var email = req.body.email;
+    var number = req.body.number;
+
+    var data = {
+        "email" : email,
+        "number" : number
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('textarea.html')
+
+})
+//---
+app.post("/sign_up",(req,res)=> {
+    var email = req.body.email;
+    var password = req.body.password;
+    var number = req.body.number;
+
+    var data = {
+        "email" : email,
+        "password" : password,
+        "number" : number
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('signin.html')
+
+})
+app.post("/sign_in",(req,res)=>{
+    var email = req.body.email;
+    var password = req.body.password;
+
+    var data = {
+        "email" : email,
+        "password" : password
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('textarea.html')
+
+})
+app.post("/reset",(req,res)=>{
+    var email = req.body.email;
+    var number = req.body.number;
+
+    var data = {
+        "email" : email,
+        "number" : number
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('textarea.html')
+
+})
+
+app.post("/textarea",(req,res)=>{
+    var email = req.body.email;
+    var comment = req.body.comment;
+
+    var data = {
+        "email" : email,
+        "comment" : comment
+    }
+
+    db.collection('comments').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('textarea.html')
+
+})
+app.get("/textarea",(req,res)=>{
+
+
+    db.collection('comments').find({},(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log(collection);
+        return res.send(collection);
+    });
+
+   // return res.send({});
+    
+})
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('signin.html');
+}).listen(3000);
+
+
+console.log("Listening on PORT 3000");
+
