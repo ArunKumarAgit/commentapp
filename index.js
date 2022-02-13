7d717f47c042bad781ca62bd4b18c3fe8a02c808
aarunkumar5044@gmail.com <aarunkumar5044@gmail.com>
2022-02-13 20:28:06 +0530

    commit

--- index.js
+++ index.js
@@ -7,81 +7,50 @@ const app = express()
 app.use(bodyParser.json())
 app.use(express.static('commentapp'))
 app.use(bodyParser.urlencoded({
-    extended:true
+    extended: true
 }))
 
-mongoose.connect('mongodb://localhost:27017/mydb',{
+mongoose.connect('mongodb://localhost:27017/mydb', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 var db = mongoose.connection;
 
-db.on('error',()=>console.log("Error in Connecting to Database"));
-db.once('open',()=>console.log("Connected to Database"));
+db.on('error', () => console.log("Error in Connecting to Database"));
+db.once('open', () => console.log("Connected to Database"));
 
-//get request
-app.get('/sign_up',async(req,res) => {
-    try{
-           const aliens = await Alien.find()
-           res.json(aliens)
-    }catch(err){
-        res.send('Error ' + err)
-    }
-})
-
-app.get("/reset",(req,res)=>{
-    var email = req.body.email;
-    var number = req.body.number;
-
-    var data = {
-        "email" : email,
-        "number" : number
-    }
-
-    db.collection('users').insertOne(data,(err,collection)=>{
-        if(err){
-            throw err;
-        }
-        console.log("Record Inserted Successfully");
-    });
-
-    return res.redirect('textarea.html')
-
-})
-//---
-app.post("/sign_up",(req,res)=> {
+app.post("/sign_up", (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
     var number = req.body.number;
 
     var data = {
-        "email" : email,
-        "password" : password,
-        "number" : number
+        "email": email,
+        "password": password,
+        "number": number
     }
 
-    db.collection('users').insertOne(data,(err,collection)=>{
-        if(err){
+    db.collection('users').insertOne(data, (err, collection) => {
+        if (err) {
             throw err;
         }
         console.log("Record Inserted Successfully");
     });
-
-    return res.redirect('signin.html')
+    return res.redirect('textarea.html')
 
 })
-app.post("/sign_in",(req,res)=>{
+app.post("/sign_in", (req, res) => {
     var email = req.body.email;
     var password = req.body.password;
 
     var data = {
-        "email" : email,
-        "password" : password
+        "email": email,
+        "password": password
     }
 
-    db.collection('users').insertOne(data,(err,collection)=>{
-        if(err){
+    db.collection('users').insertOne(data, (err, collection) => {
+        if (err) {
             throw err;
         }
         console.log("Record Inserted Successfully");
@@ -90,17 +59,17 @@ app.post("/sign_in",(req,res)=>{
     return res.redirect('textarea.html')
 
 })
-app.post("/reset",(req,res)=>{
+app.post("/reset", (req, res) => {
     var email = req.body.email;
     var number = req.body.number;
 
     var data = {
-        "email" : email,
-        "number" : number
+        "email": email,
+        "number": number
     }
 
-    db.collection('users').insertOne(data,(err,collection)=>{
-        if(err){
+    db.collection('users').insertOne(data, (err, collection) => {
+        if (err) {
             throw err;
         }
         console.log("Record Inserted Successfully");
@@ -110,17 +79,17 @@ app.post("/reset",(req,res)=>{
 
 })
 
-app.post("/textarea",(req,res)=>{
+app.post("/textarea", (req, res) => {
     var email = req.body.email;
     var comment = req.body.comment;
 
     var data = {
-        "email" : email,
-        "comment" : comment
+        "email": email,
+        "comment": comment
     }
 
-    db.collection('comments').insertOne(data,(err,collection)=>{
-        if(err){
+    db.collection('comments').insertOne(data, (err, collection) => {
+        if (err) {
             throw err;
         }
         console.log("Record Inserted Successfully");
@@ -129,27 +98,78 @@ app.post("/textarea",(req,res)=>{
     return res.redirect('textarea.html')
 
 })
-app.get("/textarea",(req,res)=>{
-
-
-    db.collection('comments').find({},(err,collection)=>{
-        if(err){
-            throw err;
-        }
-        console.log(collection);
-        return res.send(collection);
-    });
-
-   // return res.send({});
+app.get("/reset",async (req,res)=>{
     
+    try {
+
+        const collection = await mydb.collection('users').find({});
+
+        return res.json(collection);
+
+    } catch(e) {
+
+        return res.json({});
+
+    }
+
+
 })
-app.get("/",(req,res)=>{
+
+app.get("/sign_up",async (req,res)=>{
+    
+    try {
+
+        const collection = await mydb.collection('users').find({});
+
+        return res.json(collection);
+
+    } catch(e) {
+
+        return res.json({});
+
+    }
+
+
+})
+app.get("/sign_in",async (req,res)=>{
+    
+    try {
+
+        const collection = await mydb.collection('users').find({});
+
+        return res.json(collection);
+
+    } catch(e) {
+
+        return res.json({});
+
+    }
+
+
+})
+
+app.get("/textarea",async (req,res)=>{
+
+    try {
+
+        const collection = await mydb.collection('comments').find({});
+
+        return res.json(collection);
+
+    } catch(e) {
+
+        return res.json({});
+
+    }
+
+
+})
+
+app.get("/", (req, res) => {
     res.set({
         "Allow-access-Allow-Origin": '*'
     })
     return res.redirect('signin.html');
 }).listen(3000);
 
-
 console.log("Listening on PORT 3000");
-
